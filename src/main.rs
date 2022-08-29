7cb4ff58c1eb964958401c9343e05ae9eb5621a4
hh-Naram <hh.naram@gmail.com>
2022-08-29 15:31:41 +0300

    Writer

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,46 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
+#[rustfmt::skip]
+fn intro() {
+    println!("  .,-:::::  ...    :::.        :   .        :  .,:::::: :::::::..");
+    println!(",;;;'````'  ;;     ;;;;;,.    ;;;  ;;,.    ;;; ;;;;'''' ;;;;``;;;;");
+    vga_buffer::WRITER.lock().set_color(vga_buffer::ColorCode::new(vga_buffer::Color::Yellow, vga_buffer::Color::Black));
+    println!("[[[        [['     [[[[[[[, ,[[[[, [[[[, ,[[[[, [[cccc   [[[,/[[[\'");
+    vga_buffer::WRITER.lock().set_color(vga_buffer::ColorCode::new(vga_buffer::Color::LightGreen, vga_buffer::Color::Black));
+    println!("$$$        $$      $$$$$$$$$$$\"$$$ $$$$$$$$\"$$$ $$\"\"\"\"   $$$$$$c");
+    vga_buffer::WRITER.lock().set_color(vga_buffer::ColorCode::new(vga_buffer::Color::LightRed, vga_buffer::Color::Black));
+    println!("`88bo,__,o,88    .d888888 Y88\" 888o888 Y88\" 888o888oo,__ 888b \"88bo,");
+    vga_buffer::WRITER.lock().set_color(vga_buffer::ColorCode::new(vga_buffer::Color::Red, vga_buffer::Color::Black));
+    println!("  \"YUMMMMMP\"\"YmmMMMM\"\"MMM  M\'  \"MMMMMM  M\'  \"MMM\"\"\"\"YUMMMMMMM   \"W\"");
+
+    vga_buffer::WRITER.lock().set_color(vga_buffer::ColorCode::new(vga_buffer::Color::White, vga_buffer::Color::Black));
+    println!("\n       A joke operating system made for a friend by Hachem H.");
+    println!("                        Happy birthday mate\n\n");
+    vga_buffer::WRITER.lock().set_color(vga_buffer::ColorCode::new(vga_buffer::Color::LightGray, vga_buffer::Color::Black));
+    println!("To get a list of commands, start by typing `help`.");
+    println!("Or because we are cool human creatures just type ?.");
+    println!("Much shorter for you bous.\n\n");
+    vga_buffer::WRITER.lock().set_color(vga_buffer::ColorCode::new(vga_buffer::Color::White, vga_buffer::Color::Black));
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in b"Cummer OS".iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xf;
-        }
-    }
+    intro();
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    vga_buffer::WRITER
+        .lock()
+        .set_color(vga_buffer::ColorCode::new(
+            vga_buffer::Color::White,
+            vga_buffer::Color::Red,
+        ));
+    print!("{}", info);
     loop {}
 }
